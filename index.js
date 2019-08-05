cc0af290328138e5a07321e5f3e16f3b986b9c00
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-08-05 12:14:55 +0300

    Add 3.10 puhelinluettelon backend step10

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-const port = 3001
+const port = process.env.PORT || 3001
 
 let persons = [
   { 
