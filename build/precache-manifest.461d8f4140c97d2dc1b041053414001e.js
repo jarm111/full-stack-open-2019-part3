2948a54775335f982239db901acb616134d1e1ac
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-08-05 13:18:34 +0300

    uibuild

---
+++ build/precache-manifest.461d8f4140c97d2dc1b041053414001e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9cf2895b1ec46a66967267b1dcc8bdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d961e746ed7c89fde496",
+    "url": "/static/css/main.f4faf359.chunk.css"
+  },
+  {
+    "revision": "bc120ca277fc438795d3",
+    "url": "/static/js/2.dbf5eb8f.chunk.js"
+  },
+  {
+    "revision": "d961e746ed7c89fde496",
+    "url": "/static/js/main.36b6ea24.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
