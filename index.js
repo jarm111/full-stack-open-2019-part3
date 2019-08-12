91bbc5747b1a992b5ef30c647d908b384371af6e
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-08-12 12:30:20 +0300

    Add 3.13: puhelinluettelo ja tietokanta, step1

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-const port = process.env.PORT || 3001
 
-let persons = [
-  { 
-    "name": "Arto Hellas", 
-    "number": "040-123456",
-    "id": 1
-  },
-  { 
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  { 
-    "name": "Dan Abramov", 
-    "number": "12-43-234345",
-    "id": 3
-  },
-  { 
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122",
-    "id": 4
-  }
-]
+const app = express()
+const Person = require('./models/person')
+const port = process.env.PORT
+
+// let persons = [
+//   { 
+//     "name": "Arto Hellas", 
+//     "number": "040-123456",
+//     "id": 1
+//   },
+//   { 
+//     "name": "Ada Lovelace", 
+//     "number": "39-44-5323523",
+//     "id": 2
+//   },
+//   { 
+//     "name": "Dan Abramov", 
+//     "number": "12-43-234345",
+//     "id": 3
+//   },
+//   { 
+//     "name": "Mary Poppendieck", 
+//     "number": "39-23-6423122",
+//     "id": 4
+//   }
+// ]
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -48,43 +51,45 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(people => {
+    res.json(people)
+  })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(e => e.id === id)
-  person 
-    ? res.json(person)
-    : res.status(404).end()
+// app.get('/api/persons/:id', (req, res) => {
+//   const id = Number(req.params.id)
+//   const person = persons.find(e => e.id === id)
+//   person 
+//     ? res.json(person)
+//     : res.status(404).end()
 
-})
+// })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  if (!persons.some(e => e.id === id)) {
-    return res.status(404).end()
-  }
-  persons = persons.filter(e => e.id !== id)
-  res.status(204).end()
-})
+// app.delete('/api/persons/:id', (req, res) => {
+//   const id = Number(req.params.id)
+//   if (!persons.some(e => e.id === id)) {
+//     return res.status(404).end()
+//   }
+//   persons = persons.filter(e => e.id !== id)
+//   res.status(204).end()
+// })
 
-app.post('/api/persons/', (req, res) => {
-  const {name, number} = req.body
+// app.post('/api/persons/', (req, res) => {
+//   const {name, number} = req.body
 
-  if (!name || !number) {
-    return res.status(400).json({error: 'missing name or number'})
-  }
+//   if (!name || !number) {
+//     return res.status(400).json({error: 'missing name or number'})
+//   }
 
-  if (persons.some(e => e.name === name)) {
-    return res.status(400).json({error: 'name must be unique'})
-  }
+//   if (persons.some(e => e.name === name)) {
+//     return res.status(400).json({error: 'name must be unique'})
+//   }
 
-  const id = Math.floor(Math.random() * 100000) + 6;
-  const newPerson = {id, name, number}
-  persons = persons.concat(newPerson)
-  res.json(newPerson)
-})
+//   const id = Math.floor(Math.random() * 100000) + 6;
+//   const newPerson = {id, name, number}
+//   persons = persons.concat(newPerson)
+//   res.json(newPerson)
+// })
 
 app.listen(port, () => {
   console.log(`Phonebook backend running on port ${port}`)
