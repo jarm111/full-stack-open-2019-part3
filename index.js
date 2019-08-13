3fd7e15aa2c59bbb5cece80645f9bb4181ba609d
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-08-13 11:59:20 +0300

    Add 3.18*: puhelinluettelo ja tietokanta, step5

--- index.js
+++ index.js
@@ -8,29 +8,6 @@ const app = express()
 const Person = require('./models/person')
 const port = process.env.PORT
 
-// let persons = [
-//   { 
-//     "name": "Arto Hellas", 
-//     "number": "040-123456",
-//     "id": 1
-//   },
-//   { 
-//     "name": "Ada Lovelace", 
-//     "number": "39-44-5323523",
-//     "id": 2
-//   },
-//   { 
-//     "name": "Dan Abramov", 
-//     "number": "12-43-234345",
-//     "id": 3
-//   },
-//   { 
-//     "name": "Mary Poppendieck", 
-//     "number": "39-23-6423122",
-//     "id": 4
-//   }
-// ]
-
 app.use(bodyParser.json())
 app.use(cors())
 
@@ -40,14 +17,16 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(express.static('build'))
 
 app.get('/info', (req, res) => {
-  const entries = persons.length
-  const currentTime = new Date();
-  const text = `
-    <p>Phonebook has info for ${entries} people</p>
-    <p>${currentTime}</p>
-  `
-
-  res.send(text)
+  Person.find({})
+    .then(people => people ? people.length : 0)
+    .then(entries => {
+      const currentTime = new Date();
+      const text = `
+        <p>Phonebook has info for ${entries} people</p>
+        <p>${currentTime}</p>
+      `
+      res.send(text)
+    })
 })
 
 app.get('/api/persons', (req, res) => {
@@ -56,14 +35,13 @@ app.get('/api/persons', (req, res) => {
   })
 })
 
-// app.get('/api/persons/:id', (req, res) => {
-//   const id = Number(req.params.id)
-//   const person = persons.find(e => e.id === id)
-//   person 
-//     ? res.json(person)
-//     : res.status(404).end()
-
-// })
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      person ? res.json(person) : res.status(404).end()
+    })
+    .catch(error => next(error))
+})
 
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
@@ -79,10 +57,6 @@ app.post('/api/persons/', (req, res) => {
   if (!name || !number) {
     return res.status(400).json({error: 'missing name or number'})
   }
-
-  // if (persons.some(e => e.name === name)) {
-  //   return res.status(400).json({error: 'name must be unique'})
-  // }
 
   const person = new Person({
     name,
